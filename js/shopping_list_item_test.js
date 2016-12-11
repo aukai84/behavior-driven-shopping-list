063eab7924e79d3af699b5bed6bfc9e868947f9b
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-10 18:41:46 -1000

    added tests to check if shopping list is a calss, has properties, and has check method

---
+++ js/shopping_list_item_test.js
@@ -0,0 +1,29 @@
+/*jshint esversion: 6 */
+const should = chai.should();
+const expect = chai.should();
+
+
+
+describe("ShoppingListItem", () => {
+  let milk;
+
+  beforeEach(() => {
+    milk = new ShoppingListItem("Milk", "Good with cookies");
+  });
+  it("should be a class", () => {
+    milk.should.be.a.function
+  });
+  it("should have a name property", () => {
+    milk.should.have.property("name")
+  });
+  it("should haev property description", () => {
+    milk.should.have.property("description")
+  });
+  it("should have property to show if its done", () => {
+    milk.should.have.property("is_done")
+  });
+  it("should have method check", ()=> {
+    milk.check.should.be.a.function
+    milk.check().should.equal(milk.is_done = true)
+  });
+});
