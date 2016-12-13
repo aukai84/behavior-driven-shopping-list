bf9bf0d453596a47f0177e36af3c11293a7c0ed9
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-12 22:38:13 -1000

    finished remove item method.  checks if item is instance of shopping list item and located in items array, if not throw error

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -7,7 +7,7 @@ class ShoppingList {
     if(item instanceof ShoppingListItem){
     this.items.push(item);
     } else {
-      throw Error("Not an item in your list");
+      throw Error("Cannot add item that isn't a shopping list item");
    }
   }
   removeItem(item){
@@ -16,7 +16,11 @@ class ShoppingList {
     } else if(item instanceof ShoppingListItem){
       if(this.items.indexOf(item) > -1){
       this.items.splice(this.items.indexOf(item), 1);
+      } else {
+        throw Error("Cannot remove item not on your list");
       }
+    } else {
+      throw Error("Cannot remove item that isn't a shopping list item");
     }
   }
 
